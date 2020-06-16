c9b269847b76cded67b640e9941262e4c53dc1e6
Thiago Silva <thiagosilva@MacBook-Air-de-Thiago-2.local>
2020-06-16 09:03:03 -0300

    Inserindo ultimas atividades de interface do java

---
+++ POO/src/poo/Interface/Cliente.java
@@ -0,0 +1,28 @@
+package poo.Interface;
+
+/**
+ *
+ * @author thiagosilva
+ */
+public class Cliente implements IDAO {
+    
+    private int codigo;
+    private String nome;
+    
+    @Override
+    public void inserir() {
+        
+    }
+
+    @Override
+    public void excluir() {
+        
+    }
+
+    @Override
+    public void localizar() {
+        
+    }
+
+    
+}
---
+++ POO/src/poo/Interface/IDAO.java
@@ -0,0 +1,14 @@
+
+package poo.Interface;
+
+/**
+ *
+ * @author thiagosilva
+ */
+public interface IDAO {
+    public void inserir();
+    public void excluir();
+    public void localizar();
+    
+    
+}
